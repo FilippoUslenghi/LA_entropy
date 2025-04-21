26261b3c1ff1606c5c6e363e856d74dee888543e
Filippo Uslenghi <us98@hotmail.it>
2025-04-21 23:45:01 +0200

    added average edge length computation when using a resampled mesh

--- vertex_voltage_mapping.m
+++ vertex_voltage_mapping.m
@@ -3,39 +3,14 @@ function vertex_voltage_map = vertex_voltage_mapping(vertices, triangles, voltag
     r = 8;
     d = 2;
 
-    % if is_resampled
-    %     % Get neighboring vertices    
-    %     neighbors = zeros(size(Vertices,1), size(Vertices,1));
-    %     for ii = 1:size(Vertices,1)
-    %         [row,col] = find(Triangles==ii);
-    %         for jj = 1:length(row)
-    %             if col(jj)==1
-    %                 neighbors(ii, Triangles(row(jj),2)) = 1;
-    %                 neighbors(ii, Triangles(row(jj),3)) = 1;
-    %             end
-    %             if col(jj)==2
-    %                 neighbors(ii, Triangles(row(jj),1)) = 1;
-    %                 neighbors(ii, Triangles(row(jj),3)) = 1;
-    %             end
-    %             if col(jj)==3
-    %                 neighbors(ii, Triangles(row(jj),1)) = 1;
-    %                 neighbors(ii, Triangles(row(jj),2)) = 1;
-    %             end
-    %         end
-    %     end
-    % 
-    %     % Distance between vertices.
-    %     distances = zeros(size(Vertices,1), size(Vertices,1));
-    %     for ii=1:size(Vertices,1)
-    %         for jj=1:size(Vertices,1)
-    %             if neighbors(ii, jj) == 1
-    %                 distances(ii,jj) =  pdist([Vertices(ii,:); Vertices(jj,:)], 'euclidean'); %distanze solo dei vicini
-    %             end
-    %         end
-    %     end
-    %     d = 2 * mean(distances(find(distances)));
-    
-    vertices_normals = vertices + vertexNormal(triangulation(triangles, vertices));
+    TR = triangulation(triangles, vertices);
+    vertices_normals = vertices + vertexNormal(TR);
+
+    if is_resampled
+        E = edges(TR);
+        avg_edge_length = mean(sqrt(sum((TR.Points(E(:,1), :) - TR.Points(E(:,2), :)).^2, 2)));
+        d = 2 * avg_edge_length;
+    end
     
     % Sphere
     sphere_candidates = pdist2(vertices, coordinates, 'squaredeuclidean') <= r^2;
@@ -58,4 +33,3 @@ function vertex_voltage_map = vertex_voltage_mapping(vertices, triangles, voltag
     
     vertex_voltage_map = voltages' .* final_candidates;
 end
-
