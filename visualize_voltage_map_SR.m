75b6e072c1f16dab55cde89e7517e4df2ffa387f
filippouslenghi <filippo.uslenghi@studenti.unimi.it>
2025-06-17 14:48:40 +0200

    added visualize_voltage_map-SR.m to process map in SR rhythm

--- visualize_voltage_map_SR.m
+++ visualize_voltage_map_SR.m
@@ -9,17 +9,18 @@ warning('off', 'MATLAB:MKDIR:DirectoryExists')
 load("patients_rhythms.mat")
 data_dir = "processed_data";
 
-patient_ID = "46";
+patient_dir = "77";
+SR = true;
 voltage_thrs = 15;
 filtering = true;
-bind_width = "variable";
+bin_width = "variable";
 verbose = true;
 
 % Load the data
-path_to_data = strjoin([data_dir patient_ID], '/');
-load(strjoin([path_to_data "LA_info.mat"], '/'))
-load(strjoin([path_to_data "LA_mesh.mat"], '/'))
-load(strjoin([path_to_data "LA_points_data.mat"], '/'))
+path_to_data = strjoin([data_dir patient_dir], '/');
+load(strjoin([path_to_data "LA_SR_info.mat"], '/'))
+load(strjoin([path_to_data "LA_SR_mesh.mat"], '/'))
+load(strjoin([path_to_data "LA_SR_points_data.mat"], '/'))
 
 fs = double(fs);
 
@@ -46,7 +47,8 @@ coordinates = [];
 voltages = [];
 
 % Create the filters
-[b1, a1] = butter(3, (2*[30, 300])/fs);
+[b_egm, a_egm] = butter(3, (2*[30, 300])/fs);
+[b1, a1] = butter(3, (2*[10, 300])/fs);
 [b2, a2] = butter(3, (40)/fs);
 
 % For each point export
@@ -55,20 +57,20 @@ for pp = 1:length(points_IDs)
     point_ID = points_IDs(pp);
 
     ecg_signal = signals(pp, :, ecg_signal_index);
-    
+
     % Apply first filter
     filtered_ecg_signal = filtfilt(b1, a1, ecg_signal);
     abs_filtered_ecg_signal = abs(filtered_ecg_signal);
-    
+
     % Apply second filter
     ecg_activation = filtfilt(b2, a2, abs_filtered_ecg_signal);
 
     thr = 0.08;
     [~, ecg_peak_train] = findpeaks(ecg_activation, "MinPeakHeight", thr, ...
-        "MinPeakDistance", 600);
+        "MinPeakDistance", 300);
 
     % Create the windows
-    window_bounds = [-50; 100];
+    window_bounds = [-30; 100];
     ecg_windows = ecg_peak_train + window_bounds;
     ecg_windows = min(max(1,ecg_windows), 2500);
 
@@ -80,16 +82,16 @@ for pp = 1:length(points_IDs)
     % For each electrode
     for ee = 1:length(electrodes)
         electrode_index = find(strcmp(electrodes{ee}, columns));
-        
+
         egm_signal = signals(pp,:,electrode_index)';
         if filtering
-            egm_signal = filtfilt(b1, a1, egm_signal);
+            egm_signal = filtfilt(b_egm, a_egm, egm_signal);
         end
 
         % Extract the windows from the signal
         egm_windows = arrayfun(@(a,b) egm_signal(a:b), ...
             egm_windows_bounds(:,1), egm_windows_bounds(:,2), 'Unif', 0);
-        
+
         % Compute the peak to peak measure of each window
         egm_ptp = cellfun(@(x) peak2peak(x), egm_windows);
 
@@ -137,14 +139,14 @@ if AF, clim([0.05 0.24]); else, clim([0.05, 1.5]); end
 [f, entropy] = entropy_calculation(final_voltage_map, bin_width, verbose);
 
 % Write mesh to disk for meshtool
-% vtkwrite(strjoin([data_dir, patient_ID, 'LA_mesh.vtk'], '/'), 'polydata', ...
+% vtkwrite(strjoin([data_dir, patient_ID, 'LA_SR_mesh.vtk'], '/'), 'polydata', ...
 %     'triangle', vertices(:,1), vertices(:,2), vertices(:,3), triangles);
 
 % Resample mesh with meshtool
 % command = sprintf("./meshtool/meshtool resample surfmesh -msh=%s -avrg=2 " + ...
 %     "-outmsh=%s -ofmt=vtk_polydata -surf_corr=0.8", ...
-%     strjoin([data_dir, patient_ID, 'LA_mesh.vtk'], '/'), ...
-%     strjoin([data_dir, patient_ID, 'LA_mesh_resampled.vtk'], '/'));
+%     strjoin([data_dir, patient_ID, 'LA_SR_mesh.vtk'], '/'), ...
+%     strjoin([data_dir, patient_ID, 'LA_SR_mesh_resampled.vtk'], '/'));
 % [status, ~] = system(command);
 % if status ~= 0
 %     error("Meshtool exit status is non-zero.")
